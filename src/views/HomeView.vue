fb924d418159a988c2654b65d6a03a32e4f22289
yessipc <19690070@tecvalles.mx>
2023-12-05 20:10:08 -0600

    jsjkjs

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,18 @@
 <template>
+       <div id="slider">
+        <figure>
+        <img src="../assets/taller.png" alt="">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqut5SKDQEl6-Hw9o9BUYiX0Q0jbuPkVSsoET7IiJzJ3pkhJ9zd7-EBBg2WL9mvzeUOt8&usqp=CAU" alt="">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQH1udgE7olemETrVGDklXuDAas4eFr0o72lurcgCgEbv8S4-JQL-yYNRhYl0MGMvAJsN0&usqp=CAU" alt="">
+        <img src="https://lh3.googleusercontent.com/p/AF1QipPrbcULwwditvTzMyap35EJsr6Tz0RnpPH8j49n=w768-h768-n-o-v1" alt="">
+        <img src="https://www.shutterstock.com/image-vector/tyre-cartoon-car-mechanic-service-600nw-2061820022.jpg" alt="">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfkw5KGIFOpJbHUl-1ZitiRv0v-y9cCmnqseGb4ZxPYsSWWYey7DkDA8-mBzWfpzpavA0&usqp=CAU" alt="">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5VEfR8sFR_p0o5Aj9XQd_CZAFRn7HwCU-ILRji_s9J07DmsplU9x5cRpL8RSJAuA0E9s&usqp=CAU" alt="">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBuB1MpKaw5TLSLtC0Xz6nqLjtXQ3_KwGSWQ&usqp=CAU" alt="">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAdE2JdENVcn1_CRZrXlEY95EUYi1UiTDG3r49d219cJrfwXMSkRYKnfHcb17YWWNyd84&usqp=CAU" alt="">
+        <img src="https://lh3.googleusercontent.com/p/AF1QipPrbcULwwditvTzMyap35EJsr6Tz0RnpPH8j49n=w768-h768-n-o-v1" alt="">
+        </figure>
+        </div>
   <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit,<br>
   sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br>
   Ut enim ad minim veniam, quis nostrud<br>
@@ -129,4 +143,34 @@
         padding: 7px;
         font-family: 'Poppins', serif;
     }
+    /* las imágenes no van a salir del márgen de la pantalla*/
+div#slider figure img { 
+  width: 10%; 
+  float: left; 
+}
+/*el movimiento se va a mantener de forma indefinida -infinito-*/
+div#slider figure { 
+  position: relative;
+  width: 170%;
+  margin: 0;
+  left: 0;
+  text-align: left;
+  font-size: 0;
+  animation: 5s slidy infinite; 
+}
+@keyframes slidy {
+0% { left: 0%; }
+20% { left: 0%; }
+25% { left: -100%; }
+45% { left: -100%; }
+50% { left: -200%; }
+70% { left: -200%; }
+75% { left: -300%; }
+95% { left: -300%; }
+100% { left: -400%; }
+}
+  .kiko{
+  width: 1318px;
+  height: 610px;
+}
 </style>
