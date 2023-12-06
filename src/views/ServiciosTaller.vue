c09f934ca5187335641adc1e525a3696f23aa7c5
yessipc <19690070@tecvalles.mx>
2023-12-06 02:14:37 -0600

    ayudaaaaa!!!

--- src/views/ServiciosTaller.vue
+++ src/views/ServiciosTaller.vue
@@ -1,6 +1,7 @@
 <template>
   <h1><center>Nuestros servicios</center></h1>
-   
+  
+  <h1><center>TALLER MECANICO</center></h1>
    <div>
   <img src="../assets/mecanica.jpg" width="100" height="100"  style="float:left;"> 
   <div>SERVICIO DE MECÁNICA EXPRESS
@@ -9,8 +10,66 @@
 <p>adores, Llantas, Alineación y Balanceo,Lubricación y Ajuste</p>
 <p>de Niveles, Estética Automotriz</p></div> 
 </div>
+<br>
+<br>
+ 
+<div>
+  <img src="../assets/mecanica1.jpg" width="100" height="100"  style="float:left;"> 
+  <div>SERVICIOS ESPECIALIZADOS DE REPARACIÓN
+<p>Fallas complicadas, Transmisiones, Motores, Direcciones, Bombas,</p>
+<p> Sistemas Hidráulicos y Frenos de Aire, Ejes, Diferenciales, </p>
+<p>Transfer, Turbos, Inyectores, Hojalatería / Pintura y Estética,</p>
+<p>Alarmas, Chapas, Chips, Inmovilizadores, Reprogramaciones,</p>
+<p>Elevadores, Vidrios, Polarizados de seguridad, Tapicería, Climas A/C ,</p>
+<p> Torretas, Accesorios y Aditamentos Especiales.</p>
+</div>
+</div>
+<br>
+<br>
+<div>
+  <img src="../assets/mecanica2.jpg" width="100" height="100"  style="float:left;"> 
+  <div>SERVICIO PARA AUTOS PREMIUM Y POPULARES
+<p>Deja tu auto en buenas manos, somos especialistas</p>
+<p>en autos de marcas premium como: BMW, Audi, Mercedez,</p>
+<p>Lincoln, entre otras.</p>
+</div>
+</div>
+<h1><center>TALLER ELECTRICO</center></h1>
+<div>
+<img src="../assets/mecanica3.jpg" width="100" height="100"  style="float:left;"> 
+  <div>REPARACIÓN
+<p>Como expertos en el sistema eléctrico automotriz, trabajamos con altos estándares</p>
+  <p>de calidad para que tu vehículo funcione a la perfección. Contamos con tecnología </p>
+    <p> de última generación para detectar cualquier falla en el sistema eléctrico automotriz</p>
+      <p>Alternadores</p>
+        <p>Baterías</p>
+          <p>Computadora o módulos eléctronicos del vehículos</p>
+            <p>Luces</p>
+              <p>Marchas</p>
+                <p>Red de comunicación</p>
+                  <p>Sensores</p>
+</div>
+</div>
+<br>
+<br>
+<h1><center>REPARACION DE MOTOR</center></h1>
+<div>
+<img src="../assets/mecanica4.jpg" width="100" height="100"  style="float:left;"> 
+  <div>REPARACIÓN DE MOTOR
+<p>Acude con verdaderos expertos en reparaciones de motores. Nuestro personal</p>
+   <p>te proporcionará asesoría personalizada para conocer el problema y dar un diagnóstico</p>
+  <p>certero.</p>
+<p>Reparación General de Motor.</p>
+<p>Empacadas de Motor</p>
+<p>Reparaciones de Cabeza de Motor</p>
+  </div>
+  </div>
 </template>
 
 <style>
+body {
+  text-align: left;
+}
 img { float:left; }
+img { float:right; }
 </style>
